f676389c5f29ec35895ff12aeeb16bd4b6576f14
Brent-Milan <Bmilan24@gmail.com>
2017-10-16 15:45:25 -0400

    second test passing

--- src/main/java/production/CardFactory.java
+++ src/main/java/production/CardFactory.java
@@ -2,6 +2,6 @@ package production;
 
 public class CardFactory {
 	
-	int[] values = new int[13];
+	int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
 }
--- src/test/java/production/CardFactoryTest.java
+++ src/test/java/production/CardFactoryTest.java
@@ -17,8 +17,13 @@ public class CardFactoryTest {
 	@Test
 	public void arraySizeShouldEquals13() {
 		int expected = 13;
-		
 		assertEquals(expected, underTest.values.length);
+	}
+	
+	@Test
+	public void arrayShouldIncludeAllCardValues() {
+		int[] expected = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+		assertArrayEquals(expected, underTest.values);
 	}
 
 }
